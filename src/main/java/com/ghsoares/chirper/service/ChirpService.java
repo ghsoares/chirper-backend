3e54cd874f7169f7be1155a2b13a87c87542e86c
ghsoares <ghsoares99795@gmail.com>
2022-11-11 21:40:06 -0300

    Added initial services

---
+++ src/main/java/com/ghsoares/chirper/service/ChirpService.java
@@ -0,0 +1,94 @@
+package com.ghsoares.chirper.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.ghsoares.chirper.model.Chirp;
+import com.ghsoares.chirper.model.ChirpLike;
+import com.ghsoares.chirper.model.User;
+import com.ghsoares.chirper.repository.ChirpLikeRepository;
+import com.ghsoares.chirper.repository.ChirpRepository;
+import com.ghsoares.chirper.repository.UserRepository;
+
+@Service
+public class ChirpService {
+	@Autowired
+	private ChirpRepository chirpRepository;
+	
+	@Autowired
+	private ChirpLikeRepository chirpLikeRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public Optional<Chirp> createChirp(Long userId, Chirp chirp) {
+		Optional<User> user = userRepository.findById(userId);
+		if (user.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User doesn't exist", null);
+		}
+		chirp.setAuthor(user.get());
+		return Optional.of(chirpRepository.save(chirp));
+	}
+	
+	public Optional<Chirp> updateChirp(Long userId, Chirp chirp) {
+		if (userId != chirp.getAuthor().getUserId()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Trying to update a chirp from a different user", null);
+		}
+		
+		if (chirpRepository.findById(chirp.getChirpId()).isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Chirp doesn't exist", null);
+		}
+		
+		Optional<User> user = userRepository.findById(userId);
+		if (user.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User doesn't exist", null);
+		}
+	
+		return Optional.of(chirpRepository.save(chirp));
+	}
+	
+	public Optional<ChirpLike> likeChirp(Long userId, Long chirpId) {
+		Optional<Chirp> chirp = chirpRepository.findById(chirpId);
+		if (chirp.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Chirp doesn't exist", null);
+		}
+		
+		if (chirpLikeRepository.findByUserUserIdAndChirpChirpId(userId, chirpId).isPresent()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User already liked this chirp", null);
+		}
+		
+		ChirpLike like = new ChirpLike();
+		like.setChirp(chirp.get());
+		like.setUser(new User(userId));
+		return Optional.of(chirpLikeRepository.save(like));
+	}
+	
+	public Optional<ChirpLike> unlikeChirp(Long userId, Long chirpId) {
+		Optional<ChirpLike> like = chirpLikeRepository.findByUserUserIdAndChirpChirpId(userId, chirpId);
+		
+		if (like.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Either user or chirp doesn't exist", null);
+		}
+		
+		chirpLikeRepository.deleteById(like.get().getLikeId());
+		return like;
+	}
+	
+	public Optional<Chirp> deleteChirp(Long userId, Long id) {
+		Optional<Chirp> chirp = chirpRepository.findById(id);
+		if (chirp.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Chirp doesn't exist", null);
+		}
+		
+		if (userId != chirp.get().getAuthor().getUserId()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Trying to delete a chirp from a different user", null);
+		}
+		
+		chirpRepository.deleteById(id);
+		return chirp;
+	}
+}
---
+++ src/main/java/com/ghsoares/chirper/service/UserService.java
@@ -0,0 +1,73 @@
+package com.ghsoares.chirper.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.ghsoares.chirper.model.User;
+import com.ghsoares.chirper.repository.UserRepository;
+import com.ghsoares.chirper.security.SecurityUtils;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	public Optional<User> registerUser(User user) {
+		if (userRepository.findTopByUsernameOrEmail(user.getUsername(), user.getEmail())
+				.isPresent()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User already exist", null);
+		}
+		user.setPassword(SecurityUtils.encodeString(user.getPassword()));
+		return Optional.of(userRepository.save(user));
+	}
+	
+	public Optional<User> updateUser(User user) {
+		if (userRepository.findById(user.getUserId()).isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User doesn't exist", null);
+		}
+		
+		Optional<User> searchUser = userRepository.findTopByUsernameOrEmail(user.getUsername(),
+				user.getEmail());
+
+		if (searchUser.isPresent() && (searchUser.get().getUserId() != user.getUserId())) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User already exist", null);
+		}
+
+		user.setPassword(SecurityUtils.encodeString(user.getPassword()));
+		return Optional.of(userRepository.save(user));
+	}
+	
+	public Optional<User> authUser(User loginInfo) {
+		Optional<User> user = userRepository.findTopByUsernameOrEmail(loginInfo.getUsername(), loginInfo.getUsername());
+		if (user.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid username or password", null);
+		}
+		
+		if (!SecurityUtils.compareStrings(loginInfo.getPassword(), user.get().getPassword())) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid username or password", null);
+		}
+		
+		String token = SecurityUtils.generateBasicToken(loginInfo.getUsername(), loginInfo.getPassword());
+		
+		user.get().setToken(token);
+
+		return user;
+	}
+	
+	public Optional<User> deleteUser(Long id) {
+		Optional<User> user = userRepository.findById(id);
+		if (user.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User doesn't exist", null);
+		}
+		userRepository.deleteById(id);
+		return user;
+	}
+	
+	
+	
+}
