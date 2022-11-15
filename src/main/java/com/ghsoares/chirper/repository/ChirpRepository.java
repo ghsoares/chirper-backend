6d4fa29c0687afa23322bd3ebfab497dcc2b0d39
ghsoares <ghsoares99795@gmail.com>
2022-11-14 22:16:42 -0300

    Some repository changes

--- src/main/java/com/ghsoares/chirper/repository/ChirpRepository.java
+++ src/main/java/com/ghsoares/chirper/repository/ChirpRepository.java
@@ -2,6 +2,8 @@ package com.ghsoares.chirper.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -10,10 +12,14 @@ import com.ghsoares.chirper.model.Chirp;
 
 @Repository
 public interface ChirpRepository extends JpaRepository<Chirp, Long> {
-	public List<Chirp> findAllByAuthorUsername(String username);
-	public List<Chirp> findAllByTagsIn(List<String> tags);
-	@Query("SELECT COUNT(cl) FROM ChirpLike cl WHERE cl.chirp.chirpId=chirpId")
-	public Long getLikeCount(Long chirpId);
-	@Query("SELECT c FROM Chirp c WHERE c.replyOf IS NULL")
-	public List<Chirp> findAllMain();
+	public Page<Chirp> findAllByAuthorUsernameContainingIgnoreCase(String username, Pageable pageable);
+	public Page<Chirp> findAllByTagsIn(List<String> tags, Pageable pageable);
+	
+	@Query("SELECT c FROM Chirp c WHERE replyOf IS NULL")
+	public Page<Chirp> findAllNotReply(Pageable pageable);
+	@Query("SELECT c FROM Chirp c WHERE LOWER(c.author.profileName) LIKE CONCAT('%',LOWER(:query),'%') OR LOWER(c.author.username) LIKE CONCAT('%',LOWER(:query),'%') OR LOWER(c.body) LIKE CONCAT('%',LOWER(:query),'%')")
+	public Page<Chirp> findAllByQuery(String query, Pageable pageable);
 }
+
+
+
--- src/main/java/com/ghsoares/chirper/repository/UserRepository.java
+++ src/main/java/com/ghsoares/chirper/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.ghsoares.chirper.repository;
 
-import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.ghsoares.chirper.model.User;
@@ -14,7 +16,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	public Optional<User> findTopByUsernameOrEmail(String username, String email);
 	public Optional<User> findByUsername(String username);
 	public Optional<User> findByEmail(String email);
-	public List<User> findAllByProfileNameContainingIgnoreCase(String profileName);
-	public List<User> findAllByUsernameContainingIgnoreCase(String username);
-	
+	public Page<User> findAllByProfileNameContainingIgnoreCase(String profileName, Pageable pageable);
+	public Page<User> findAllByUsernameContainingIgnoreCase(String username, Pageable pageable);
+	@Query("SELECT u FROM User u WHERE LOWER(u.profileName) LIKE CONCAT('%',LOWER(:query),'%') OR LOWER(u.username) LIKE CONCAT('%',LOWER(:query),'%')")
+	public Page<User> findAllByQuery(String query, Pageable pageable);
 }
